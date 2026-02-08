c6e70236d0d08cb0604cc99de2a1b83c18a061ca
kaiva-morphin <kaivamorphin@gmail.com>
2026-02-08 15:25:09 +0300

    one entrypoint

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,8 @@
+use bevy_asset_loader::loading_state::{LoadingState, LoadingStateAppExt};
+use bevy_asset_loader::loading_state::config::ConfigureLoadingState;
+
+use crate::games::*;
+use crate::games::plugin::AppState;
 use crate::prelude::*;
 
 use crate::core::plugin::CorePlugin;
@@ -10,7 +15,55 @@ pub mod games;
 
 fn main() {
     App::new()
-        .add_plugins(CorePlugin::default())
-        // .add_plugins(tilemap::plugin::MapPlugin)
+        .add_plugins((
+            CorePlugin::default(),
+            GamesPlugin
+        ))
         .run();
+}
+
+
+struct GamesPlugin;
+#[cfg(feature="yaro")]
+impl Plugin for GamesPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .init_state::<AppState>()
+            .add_loading_state(
+                LoadingState::new(AppState::LoadingAssets)
+                    .continue_to_state(AppState::Platformer)
+                    .load_collection::<pacman_eat::plugin::PacmanEatAssets>()
+                    .load_collection::<flappy_bird::plugin::FlappyBirdAssets>()
+                    .load_collection::<platformer::plugin::PlatformerAssets>()
+            )
+            .add_plugins((
+                pacman_eat::plugin::PacmanEatPlugin,
+                flappy_bird::plugin::FlappyBirdPlugin,
+                platformer::plugin::PlatformerPlugin,
+            ))
+           .add_systems(Update, bevy::dev_tools::states::log_transitions::<AppState>)
+           ;
+    }
+}
+
+#[cfg(feature="kaiv")]
+impl Plugin for GamesPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .init_state::<AppState>()
+            .add_loading_state(
+                LoadingState::new(AppState::LoadingAssets)
+                    .continue_to_state(AppState::Platformer)
+                    .load_collection::<pacman_eat::plugin::PacmanEatAssets>()
+                    .load_collection::<flappy_bird::plugin::FlappyBirdAssets>()
+                    .load_collection::<platformer::plugin::PlatformerAssets>()
+            )
+            .add_plugins((
+                pacman_eat::plugin::PacmanEatPlugin,
+                flappy_bird::plugin::FlappyBirdPlugin,
+                platformer::plugin::PlatformerPlugin,
+            ))
+           .add_systems(Update, bevy::dev_tools::states::log_transitions::<AppState>)
+           ;
+    }
 }
